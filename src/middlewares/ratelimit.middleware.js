a7c5c2f2234bbaa683e11acb7add8f94a93ee791
Nisarghere <nisarg72020@gmail.com>
2026-08-03 14:49:13 +0530

    commit

--- src/middlewares/ratelimit.middleware.js
+++ src/middlewares/ratelimit.middleware.js
@@ -1,3 +1,14 @@
+const { createClient } = require("redis")
+
+const redisClient = createClient({
+  url: "redis://localhost:6379"
+})
+
+redisClient.on("error", (err)=>{
+    console.log("Redis Error:", err)
+})
+await redisClient.connect();
 exports.rateLimiterMW = async(req, res) => {
+
     
 }
