c0c8aa87e089cd61eb186ba94467a9f008dd39cf
Nisarghere <nisarg72020@gmail.com>
2026-09-02 23:54:42 +0530

    commit

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ connectDb();
 
 app.use(express.static(__dirname));
 
-// app.get('/docs/:apiId', (req, res, next)=>{
-//   const apiId = req.params.apiId
-
-//   apiReference({
-//     spec:{
-//       url:`/api/openapi/${apiId}`
-//     }
-//   })(req, res, next);
-// })
+ 
 
 app.listen(5000, () => {
   console.log("app is listening on port 5000...");
