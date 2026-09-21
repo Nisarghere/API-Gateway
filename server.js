d069fe50ad34cf029ca49c6f9641ec805b4f71fc
Nisarghere <nisarg72020@gmail.com>
2026-09-22 00:13:50 +0530

    jason file updated

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ connectDb();
 app.use(express.static(__dirname));
 
  
+const PORT = process.env.PORT || 5000
 
 app.listen(5000, () => {
-  console.log("app is listening on port 5000...");
+  console.log(`app is listening on port ${PORT}...`);
 });
